2ca32de28bfcbdb138dfd2b0a53d053471b971ee
gee <xo.devgee@gmail.com>
2025-11-27 01:32:01 +0900

    fix: use host property in render.yaml and add protocol in socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,10 @@ import { io } from 'socket.io-client';
 
 // Connect to the server
 // In production, this should be set via VITE_SERVER_URL
-const URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3077';
+let URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3077';
+if (URL && !URL.startsWith('http')) {
+    URL = `https://${URL}`;
+}
 
 export const socket = io(URL, {
     autoConnect: false
