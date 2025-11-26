e0096f38228ed68f77b1c35ac35aa73b55ce3884
gee <xo.devgee@gmail.com>
2025-11-27 01:44:04 +0900

    fix: remove invalid env var and revert socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,10 +2,7 @@ import { io } from 'socket.io-client';
 
 // Connect to the server
 // In production, this should be set via VITE_SERVER_URL
-let URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3077';
-if (URL && !URL.startsWith('http')) {
-    URL = `https://${URL}`;
-}
+const URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3077';
 
 export const socket = io(URL, {
     autoConnect: false
